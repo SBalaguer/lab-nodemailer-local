6616f4c3c70895174856bbb48f1d7bf2f5095e8b
Santi Balaguer <santiago.balaguer@gmail.com>
2019-11-21 17:51:26 +0000

    almost done

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -4,6 +4,16 @@ const router = new Router();
 const User = require('./../models/user');
 const bcryptjs = require('bcryptjs');
 
+const generateId = length => {
+  const characters =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let token = '';
+  for (let i = 0; i < length; i++) {
+    token += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return token;
+};
+
 router.get('/', (req, res, next) => {
   res.render('index');
 });
@@ -12,7 +22,23 @@ router.get('/sign-up', (req, res, next) => {
   res.render('sign-up');
 });
 
+const nodemailer = require('nodemailer');
+
+const EMAIL = 'santi.ironhack.test@gmail.com';
+const PASSWORD = 's@anti1234';
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: EMAIL,
+    pass: PASSWORD
+  }
+});
+
 router.post('/sign-up', (req, res, next) => {
+  
+  const token = generateId(14);
+
   const { name, email, password } = req.body;
   bcryptjs
     .hash(password, 10)
@@ -20,16 +46,34 @@ router.post('/sign-up', (req, res, next) => {
       return User.create({
         name,
         email,
-        passwordHash: hash
+        passwordHash: hash,
+        status: 'Pending Confirmation',
+        confirmationCode: token
       });
     })
     .then(user => {
       req.session.user = user._id;
       res.redirect('/');
     })
+    .then(
+      transporter.sendMail({
+      from: `IH Test <${EMAIL}>`,
+      to: req.body.email,
+      subject: 'Test email',
+      //text: 'This should be the body of the text email'
+      html: `
+        <style>
+          h1 {
+            /* color: green !important; */
+          }
+        </style>
+        <h1 style="color: green">This should be the body of the text email</h1>
+        <p><strong>Hello</strong> <em>World!</em></p>
+      `
+    }))
     .catch(error => {
       next(error);
-    });
+    })
 });
 
 router.get('/sign-in', (req, res, next) => {
